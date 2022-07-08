771687ddffe8e6fb9e1ae93bef7ab4df0a28374e
tohage <avgorin89@gmail.com>
2022-07-08 11:21:55 +0300

    second commit

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,6 +1,24 @@
-public class Book extends  Product{
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book extends Product{
 
     private String author;
 
+    public Book( int id, String title, String author, int price) {
+        super(id,title,price);
+        this.author = author;
+    }
+    @Override
+    public boolean matches(String search) {
+        if (super.matches(search)) {
+            return true;
+        }
+        return author.contains(search);
+    }
 
 }
--- src/main/java/ProductRepository.java
+++ src/main/java/ProductRepository.java
@@ -1,14 +1,30 @@
 public class ProductRepository {
 
-    private PuroductItem[] items = new PuroductItem[0];
+    Product[] products = new Product[0];
 
-
-    public void add(Product person) {
-        Product[] tmp = new Product[Product.length + 1];
-        for (int i = 0; i < Product.length; i++) {
-            tmp[i] = Product[i];
+    public void save(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
         }
-        tmp[tmp.length - 1] = person;
-        Product = tmp;
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public void removeById(int id) {
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : products) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        products = tmp;
     }
 }
